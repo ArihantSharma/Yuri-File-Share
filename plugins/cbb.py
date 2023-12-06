d6ec1c04aa4d4c8e97ec06316a1b549b1d4394f3
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2023-12-06 23:57:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>× ɢᴏᴅ : <a href='tg://user?id={OWNER_ID}'>🫨 🫨</a>\n× ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : @Anime_Yugen\n× ᴀɴɪᴍᴇ ɢʀᴏᴜᴘ : @Anime_Yugen_Group\n× ᴍᴀɴɢᴀ ᴄʜᴀɴɴᴇʟ : @Manga_Yugen\n× ᴍᴀɴɢᴀ ɢʀᴏᴜᴘ : @Manga_Yugen_Group</b>",
+            text = f"<b>× ɢᴏᴅ : <a href='tg://user?id={OWNER_ID}'>🫨 🫨</a>\n× ʜᴇɴᴛᴀɪ ʏᴜɢᴇɴ : @Hentai_Yugen\n× ᴘᴏʀɴʜᴡᴀ ʏᴜɢᴇɴ : @Pornhwa_Yugen\n× ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : @Anime_Yugen\n× ᴍᴀɴɢᴀ ᴄʜᴀɴɴᴇʟ : @Manga_Yugen</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
@@ -26,3 +26,4 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.message.reply_to_message.delete()
         except:
             pass
+            
